c21f1be56780d14c73b4990dc2b62905d9189970
Alexandrov <alex.44747.alex@gmail.com>
2023-10-19 13:48:38 +0300

    empty

---
+++ src/main/java/ru/coffee_as_a_gift_coffee/CoffeeApplication.java
@@ -0,0 +1,13 @@
+package ru.coffee_as_a_gift_coffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/coffee_as_a_gift_coffee/CoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.coffee_as_a_gift_coffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
